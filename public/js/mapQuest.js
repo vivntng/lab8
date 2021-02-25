6501e5f0ca59b61b13f7d5a3ed28e385409c0db7
vivntng <vivntng@users.noreply.github.com>
2021-02-25 10:29:31 -0800

    completed lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'U6zo1mVZF2PL6FCXxqkRIkl3BwT3aV0Z';
+
+	// 'map' refers to a <div> element with the ID map
+	var map = L.mapquest.map('map', {
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88107584701445, -117.23751560987327]).addTo(map);
 }
